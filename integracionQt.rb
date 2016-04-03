b398b0e7bd5fd283bf7d39ad8ea9213d5bca9a7a
Angel <angarciaba@gmail.com>
2016-04-02 19:47:19 -0500

    v 0.2

--- integracionQt.rb
+++ integracionQt.rb
@@ -5,11 +5,11 @@ Copyright =
 "Autor: Angel Garcia Baños\n" +
 "Email: angel.garcia@correounivalle.edu.co\n" +
 "Fecha creación: 2016-03-29\n" +
-"Fecha última modificación: 2016-03-29\n" +
+"Fecha última modificación: 2016-04-02\n" +
 "Licencia: GNU-GPL"
-Version = "0.1"
+Version = "0.2"
 Descripcion = 
-"Toma uno o más archivos gui.ui con la descripción de una interfase de Qt5 generada con qtcreator, y uno o más archivos funciones.rb con las funciones escritas en modo procedural (ingenuo, no-OO) y los integra en una aplicacion.ui. El nombre de cada archivo con funciones debe de coincidir con el nombre de cada archivo con la respectiva interfase gui."
+"Toma uno o más archivos gui.ui con la descripción de una interfase de Qt5 generada con qtcreator, y uno o más archivos funciones.rb con las funciones escritas en modo procedural (ingenuo, no-OO) y los integra en una aplicacion.ui. El nombre de cada archivo con funciones debe de coincidir con el nombre de cada archivo que contenga la respectiva interfase gui."
 Dependencias =
 "
 sudo apt-get install ruby-qt4 qtcreator cmake
@@ -38,11 +38,8 @@ require 'ruby-prof'
 RubyProf.start
 end
 #-----------------------------------------------------------------------------------------------------------------------
-# Hay que convertir los *.ui en *.rb con el comando rbuic4. 
-# En los archivos ui hay que reconocer este tipo de lineas:
-#   <widget class="QPushButton" name="pushButton">
-#   <slot>lanzarMoneda()</slot>
-# Y en los archivos rb hay que reconocer este tipo de líneas:
+# Hay que convertir los *.ui en *_ui.rb con el comando rbuic4. 
+# En los archivos _ui.rb hay que reconocer este tipo de líneas:
 #    attr_reader :pushButton
 #    Qt::Object.connect(@pushButton, SIGNAL('clicked()'), mainWindow, SLOT('lanzarMoneda()'))
 # para extraer los atributos como pushButton y los SLOTs como lanzarMoneda
@@ -52,20 +49,9 @@ DirectorioEjecutable = "ejecutable"
 
 
 class IntegracionQt
-  def initialize(archivosEntrada, directorioSalida, archivoSalida, metodoExtraccion, comandoRubyQt="rbuic4")
-    @archivosEntrada, @directorioSalida, @archivoSalida, @metodoExtraccion, @comandoRubyQt = archivosEntrada, directorioSalida, archivoSalida, metodoExtraccion, comandoRubyQt
+  def initialize(archivosEntrada, directorioSalida, archivoSalida, comandoRubyQt="rbuic4")
+    @archivosEntrada, @directorioSalida, @archivoSalida, @comandoRubyQt = archivosEntrada, directorioSalida, archivoSalida, comandoRubyQt
     @dependencias = {}
-    @metodos = 
-    [
-      ["ui",    ".ui", /^\s*<(?:widget|layout) class=[^\s]+\s+name=["|'](\w+)["|']\/?>/, /^\s*<slot>(.+?)<\/slot>/],
-      ["rb", "_ui.rb", /^\s*attr_reader\s*:(\w*)/, /^\s*Qt::Object\.connect\(@?\w+,\s*SIGNAL\(.*?\),\s*\w+,\s*SLOT\('(.+?)'\)\)/]
-    ]
-  end
-  
-  
-  def generarNuevaExtension(viejaExtension)
-    @metodos.each { |caso| return caso[1] if caso[0] == viejaExtension }
-    nil
   end
   
   
@@ -86,24 +72,16 @@ class IntegracionQt
         $error.aviso("Los archivos .rb #{archivosFunciones} contienen funciones que no son de ningún archivo GUI. Se añadirán al final del archivo de salida")
       end
           
-      @dependencias[archivoGUI] = {}
-
-      @metodos.each do |metodo, extension, regExpAtributo, regExpSlot| 
-        if @metodoExtraccion.include?(metodo)
-          atributosGUI, slotsGUI = extraerAtributosYSlots(archivoGUI+extension, regExpAtributo, regExpSlot)
-          @dependencias[archivoGUI][metodo] = [hayFunciones, atributosGUI.uniq, slotsGUI.uniq]
-        end
-      end
+      atributosGUI, slotsGUI = extraerAtributosYSlots(archivoGUI+"_ui.rb")
+      @dependencias[archivoGUI] = [hayFunciones, atributosGUI.uniq, slotsGUI.uniq]
     end
     
     # Reporte de lo encontrado
-    @dependencias.each do |archivo, metodos|
-      metodos.each do |metodo, value|
-        p "====================\nEl archivo #{archivo}#{generarNuevaExtension(metodo)} contiene los siguientes objetos gráficos:"
-        value[1].each { |atributo| p "    #{atributo}" }
-        p "====================\nEl archivo #{archivo}#{generarNuevaExtension(metodo)} contiene las siguientes funciones (slots):"
-        value[2].each { |slot| p "    #{slot}" }
-      end
+    @dependencias.each do |archivo, value|
+      puts "====================\nEl archivo #{archivo}_ui.rb contiene los siguientes objetos gráficos:"
+      value[1].each { |atributo| puts "    #{atributo}" }
+      puts "====================\nEl archivo #{archivo}_ui.rb contiene las siguientes funciones (slots):"
+      value[2].each { |slot| puts "    #{slot}" }
     end
     
     # ToDo crear el archivo de salida
@@ -114,9 +92,9 @@ class IntegracionQt
     archivosGUI = []
     archivosFunciones = []
     @archivosEntrada.each do |archivo|
-      if archivo.end_with?("ui")
+      if archivo.end_with?(".ui")
         archivosGUI << archivo.chomp(".ui")
-      elsif archivo.end_with?("rb")
+      elsif archivo.end_with?(".rb")
         archivosFunciones << archivo.chomp(".rb")
       end
     end
@@ -124,16 +102,13 @@ class IntegracionQt
   end
   
   
-  def extraerAtributosYSlots(archivo, regExpAtributo, regExpSlot)
+  def extraerAtributosYSlots(archivo)
     atributosGUI = []
     slotsGUI = []
     open(archivo).each do |linea|
-if linea.include? "statusBar"
-p "*** #{linea}" 
-end
-      encontrado = regExpAtributo.match(linea)
+      encontrado = /^\s*attr_reader\s*:(\w*)/.match(linea)
       atributosGUI << encontrado[1] if encontrado
-      encontrado = regExpSlot.match(linea)
+      encontrado = /^\s*Qt::Object\.connect\(@?\w+,\s*SIGNAL\(.*?\),\s*\w+,\s*SLOT\('(.+?)'\)\)/.match(linea)
       slotsGUI << encontrado[1] if encontrado
     end
     return atributosGUI, slotsGUI
@@ -149,14 +124,9 @@ class Argumentos < Hash
     opciones = OptionParser.new do |opciones|
       opciones.banner = "Uso: #$0 [opciones] archivos...\n\n" + Descripcion + "\n\n" + Copyright + "\nVersion: " + Version + "\nOpciones:\n" + "Dependencias:\n" + Dependencias
       self[:salida] = "salida.rb"
-      self[:metodoExtraccion] = "ui,rb"
       
       opciones.on('-o=ARG', '--salida=ARG', "archivo de salida (el ejecutable). Por defecto es salida.rb. Y siempre se salva en el directorio #{DirectorioEjecutable}") do |argumento|
         self[:salida] = argumento
-      end
-
-      opciones.on('-m=ARG', '--metodo-extraccion=ARG', 'se extraen los atributos y slots a partir del archivo {ui, rb}. Por defecto es con ambos métodos y verificando después que coinciden. Si se selecciona un único método, no se hace ninguna verificación. Esto puede servir para intentar por uno u otro método cuando deje de funcionar uno de ellos en las nuevas versiones de Qt') do |argumento|
-        self[:metodoExtraccion] = argumento if (argumento.include?("ui") or argumento.include?("rb")) 
       end
 
       opciones.on('-b', '--verbose', 'muestra todos los errores y problemas encontrados') do
@@ -209,7 +179,7 @@ if $0 == __FILE__
   $error = Error.instance
   argumentos = Argumentos.new(ARGV)
   $error.verbose = argumentos[:verbose]
-  integracionQt = IntegracionQt.new(ARGV, DirectorioEjecutable, argumentos[:salida], argumentos[:metodoExtraccion], "rbuic4")
+  integracionQt = IntegracionQt.new(ARGV, DirectorioEjecutable, argumentos[:salida], "rbuic4")
   integracionQt.generarEjecutable
 end
 
